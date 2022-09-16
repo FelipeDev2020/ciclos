b381086f4687020fd40ee275764594b5fd8bf084
FelipeDev2020 <luisfelipexv20@gmail.com>
2022-09-15 21:44:57 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ciclos {
+    class Pessoa {
+        //Atribudos ou campos
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+
+        public void Mostrar() {
+            Console.WriteLine( string.Format($"Meu nome é {nome} {sobrenome} e meu ano de nascimento é {anoNascimento}"));
+        }
+
+        public void Somar(int num1, int num2) {
+            int resultado = num1 + num2;
+            Console.WriteLine("A soma é: " + resultado);
+        }
+
+        public void Apresentar(String nome, int idade) {
+            Console.WriteLine("Meu nome é " + nome + " e tenho " + idade + " anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ciclos {
+    class Program {
+        static void Main(string[] args) {
+
+            //Pessoa p1 = new Pessoa();
+            //p1.nome = Console.ReadLine();
+            //p1.sobrenome = Console.ReadLine();
+            //p1.anoNascimento = int.Parse(Console.ReadLine());
+
+            //p1.Mostrar();
+            
+            //p1.Somar(25, 50);
+
+            //p1.Apresentar("Felipe", 24);
+        }
+    }
+
+}
